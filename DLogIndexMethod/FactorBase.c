0b7618c6c1e2a5a4d758b85c81dc170d7e7d1d77
Andrea Graziani <andrea.graziani93@outlook.it>
2019-11-21 11:31:50 +0100

    update

--- DLogIndexMethod/FactorBase.c
+++ DLogIndexMethod/FactorBase.c
@@ -6,8 +6,80 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <pthread.h>
 #include "FactorBase.h"
 #include "../Math/Number.h"
+
+typedef struct {
+
+    __mpz_struct **buffer;
+    unsigned int head;
+    unsigned int tail;
+    unsigned int bufferSize;
+
+    pthread_cond_t pthreadCondition;
+    pthread_mutex_t pthreadMutex;
+
+} PrimeNumberBuffer;
+
+
+PrimeNumberBuffer *allocatePrimeNumberBuffer() {
+
+    PrimeNumberBuffer *output = malloc(sizeof(FactorBaseNode));
+    if (output == NULL)
+        exit(EXIT_FAILURE);
+    else {
+
+        if (pthread_mutex_init(&output->pthreadMutex, NULL) != 0)
+            exit(EXIT_FAILURE);
+
+        if (pthread_cond_init(&output->pthreadCondition, NULL) != 0)
+            exit(EXIT_FAILURE);
+
+        output->head = 0;
+        output->tail = 0;
+        output->bufferSize = 1000;
+
+        output->buffer = malloc(output->bufferSize * sizeof(__mpz_struct *));
+        if (output->buffer == NULL)
+            exit(EXIT_FAILURE);
+        else
+            return output;
+    }
+}
+
+void primeNumberBufferPush(PrimeNumberBuffer *buffer, __mpz_struct *data) {
+
+    pthread_mutex_lock(&buffer->pthreadMutex);
+
+    if (((buffer->head + 1) % buffer->bufferSize) == buffer->tail)
+        pthread_cond_wait(&buffer->pthreadCondition, &buffer->pthreadMutex);
+
+
+    *(buffer->buffer + buffer->head) = data;
+    buffer->head = (buffer->head + 1) % buffer->bufferSize;
+
+    pthread_mutex_unlock(&buffer->pthreadMutex);
+    pthread_cond_signal(&buffer->pthreadCondition);
+
+}
+
+__mpz_struct *primeNumberBufferPop(PrimeNumberBuffer *buffer, __mpz_struct *data) {
+
+    pthread_mutex_lock(&buffer->pthreadMutex);
+
+    if (buffer->head == buffer->tail)
+        pthread_cond_wait(&buffer->pthreadCondition, &buffer->pthreadMutex);
+
+    __mpz_struct *output = *(buffer->buffer + buffer->tail);
+    buffer->tail = (buffer->tail + 1) % buffer->bufferSize;
+
+    pthread_mutex_unlock(&buffer->pthreadMutex);
+    pthread_cond_signal(&buffer->pthreadCondition);
+
+    return output;
+}
+
 
 FactorBaseNode *allocateFactorBaseNode(__mpz_struct *primeNumber) {
 
